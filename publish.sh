988198312c10e19ee371867c2f3a6cca2cf9530d
Naveen Tirumalasetty <tsnaveen@hotmail.com>
2017-11-28 23:38:10 -0700

    trying

--- publish.sh
+++ publish.sh
@@ -11,8 +11,6 @@ set -e
 set -o pipefail
 
 # Set tag on git repo
-git remote set-url origin git@bitbucket.org:user/repo
-git remote set-url origin git@github.com:tsnaveen/pip-services-quotes-dotnet.git
 git tag $TAG
 git push --tags git@github.com:tsnaveen/pip-services-quotes-dotnet
 
